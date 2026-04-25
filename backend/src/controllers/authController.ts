aee92bf16dfbbd65a86be0ba310b4c6c8df00372
Its-Delimas <spencerdelimas@gmail.com>
2026-04-25 13:19:41 +0300

    feat: login authentication logic controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -38,3 +38,37 @@ export const register = async (req:Request, res:Response)=>{
         res.status(500).json({message:'Server error'})
     }
 }
+
+export const login = async(req:Request, res:Response)=>{
+    try {
+        const {email, password} = req.body
+
+        const user = await User.findOne({email})
+        if(!user){
+            res.status(400).json({message:'Invalid credentials'})
+            return
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            res.status(400).json({message:"Invalid credentials"})
+            return
+        }
+
+        const token = jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET!
+            {expiresIn:'7d'}
+        )
+
+        res.status(200).json({
+            token,
+            user:{
+                id:user._id,
+                email:user.email
+            }
+        })
+    } catch (err){
+        res.status(500).json({message:'Server error'})
+    }
+}
